106d85d1fba92dd19e81d53916ba586130147194
Antonis <antonis.papath@gmail.com>
2014-05-22 21:43:03 +0300

    ignore

---
+++ gen_static_data.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html >
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="">
+<meta name="description" content="">
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+
+<link rel="shortcut icon" href="" />
+
+<title>MMSCH</title>
+
+
+<script type="text/javascript" src="client/vendors/kendo/js/jquery.min.js"></script>
+<script type="text/javascript" src="client/js/jquery-ui.custom.min.js"></script>
+<script type="text/javascript" src="client/vendors/kendo/js/kendo.web.min.js"></script>
+<script type="text/javascript" src="client/vendors/kendo/js/cultures/kendo.culture.el-GR.min.js"></script>
+  
+
+<script type="text/javascript" src="client/js/webtoolkit.base64.js"></script>
+<script type="text/javascript" src="client/dataSources.js"></script>
+<script type="text/javascript" src="client/generate.static.data.js"></script>
+
+
+
+
+
+<!--[if lt IE 7]>
+   <style type="text/css">
+        #wrapper { height:100%; }
+    </style>
+<![endif]-->
+
+<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<script>
+$(document).ready(function() {
+	
+	
+	loadStaticData();
+
+	
+	
+});
+
+</script>
+
+</head>
+
+<body>
+
+
+</body>
+</html>
