8e51b0905930ded7f91978e4fdcdac50cdaf12c6
Dimosthenis Nikoudis <dnna@dnna.gr>
2014-06-12 16:17:34 +0300

    sync_survey_units dictionary search fixes

--- server/sync/sync_survey_units.php
+++ server/sync/sync_survey_units.php
@@ -495,7 +495,7 @@ class UnitsParseListener implements \JsonStreamingParser_Listener {
 
     function convert_greek_accents($str) {
         $unwanted_array = array('Ά' => 'Α', 'ά' => 'α', 'Έ' => 'Ε', 'έ' => 'ε', 'Ή' => 'Η', 'ή' => 'η', 'Ί' => 'Ι', 'ί' => 'ι', 'Ό' => 'Ο', 'ό' => 'ο', 'Ύ' => 'Υ', 'ύ' => 'υ', 'Ώ' => 'Ω', 'ώ' => 'ω', 'ϊ' => 'ι','ϋ' => 'υ', 'Ϊ' => 'Ι', 'Ϋ' => 'Υ');
-        $str = str_replace(array_keys($unwanted_array), array_values($unwanted_array), $str);
+        $str = mb_strtoupper(str_replace(array_keys($unwanted_array), array_values($unwanted_array), $str), 'UTF-8');
         return $str;
     }
 }
