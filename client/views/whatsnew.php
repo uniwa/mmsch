f3de87538509fb5d748d316503cb07df3cfed5d5
Antonis <antonis.papath@gmail.com>
2014-09-12 22:31:24 +0300

    fix whats new

--- client/views/whatsnew.php
+++ client/views/whatsnew.php
@@ -1,5 +1,14 @@
+<?php 
+function file_get_contents_utf8($fn) {
+	$content = file_get_contents($fn);
+	return mb_convert_encoding($content, 'UTF-8',
+			mb_detect_encoding($content, 'UTF-8, ISO-8859-7', true));
+}
 
+$whatsnew = trim(file_get_contents_utf8('../../whatsnew.txt'));
+?>
 <textarea id="editor" rows="10" cols="30" style="width:100%;height:height:100%">
+<?php echo $whatsnew; ?>
 </textarea>
 
 
