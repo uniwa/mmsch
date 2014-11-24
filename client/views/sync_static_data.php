3b69920b7d24f7b92b897399d57ef13703bec5a8
Antonis <antonis.papath@gmail.com>
2014-11-25 00:21:12 +0200

    improve feedback for sync static data

--- client/views/sync_static_data.php
+++ client/views/sync_static_data.php
@@ -1,6 +1,11 @@
 <div id="mod-sync-static-data" class="main-pane" style="position: absolute; left:0; top:0; bottom:0; height: 100%; width:100%;">
 	<center>
 		<button id="btnSyncStaticData" class="k-button btn-lg">Sync Static Data</button>
+
+		<p id="txtNoteStaticData" class="hidden">Τα static data ενημερώθηκαν.</p>
+		<textarea id="txtStaticData" rows="10" cols="30" style="width:80%;height:height:80%" disabled></textarea>
+		
+
 	</center>
 </div>
 
@@ -14,13 +19,28 @@ $(document).ready(function() {
 	
 	var staticData = new Object();
 
+	var callbackSuccessGetStaticData = function(){
+		
+	};
+	
+	var callbackFailGetStaticData = function(){
+	};
+	
 	function fillStaticData(lexicalName, data){
 		staticData[lexicalName] = new Object();
 		staticData[lexicalName]['total'] = data.length;
 		staticData[lexicalName]['data'] = data;
 	}
 
+	$("#txtStaticData").kendoEditor({tools: [], encoded: false});
+
+	var kui_txtStaticData = $('#txtStaticData').data('kendoEditor');
+	kui_txtStaticData.body.contentEditable=false;
+
 	$("body").on("click", "#btnSyncStaticData", function(e){
+
+		kui_txtStaticData.value(null);
+		$("#txtNoteStaticData").addClass("hidden");
 		
 		e.preventDefault();
         $(this).button('loading');
@@ -62,25 +82,26 @@ $(document).ready(function() {
         				SubnetTypes,
         				CircuitTypes ) {
 
-			fillStaticData('Prefectures', Prefectures[0].data);
-        	fillStaticData('Municipalities', Municipalities[0].data);
-        	fillStaticData('RegionEduAdmins', RegionEduAdmins[0].data);
-        	fillStaticData('EduAdmins', EduAdmins[0].data);
-        	fillStaticData('ImplEnt', ImplEnt[0].data);
-        	fillStaticData('UnitTypes', UnitTypes[0].data);
-        	fillStaticData('ΕducationLevels', ΕducationLevels[0].data);
-        	fillStaticData('Categories', Categories[0].data);
-        	fillStaticData('States', States[0].data);
-        	fillStaticData('Sources', Sources[0].data);
-        	fillStaticData('OrientationTypes', OrientationTypes[0].data);
-        	fillStaticData('OperationShifts', OperationShifts[0].data);
-        	fillStaticData('SpecialTypes', SpecialTypes[0].data);
-        	fillStaticData('TransferAreas', TransferAreas[0].data);
-        	fillStaticData('LegalCharacters', LegalCharacters[0].data);
-        	fillStaticData('SubnetTypes', SubnetTypes[0].data);
-        	fillStaticData('CircuitTypes', CircuitTypes[0].data);
+				fillStaticData('Prefectures', Prefectures[0].data);
+        		fillStaticData('Municipalities', Municipalities[0].data);
+        		fillStaticData('RegionEduAdmins', RegionEduAdmins[0].data);
+        		fillStaticData('EduAdmins', EduAdmins[0].data);
+	        	fillStaticData('ImplEnt', ImplEnt[0].data);
+	        	fillStaticData('UnitTypes', UnitTypes[0].data);
+	        	fillStaticData('ΕducationLevels', ΕducationLevels[0].data);
+	        	fillStaticData('Categories', Categories[0].data);
+	        	fillStaticData('States', States[0].data);
+	        	fillStaticData('Sources', Sources[0].data);
+	        	fillStaticData('OrientationTypes', OrientationTypes[0].data);
+	        	fillStaticData('OperationShifts', OperationShifts[0].data);
+	        	fillStaticData('SpecialTypes', SpecialTypes[0].data);
+	        	fillStaticData('TransferAreas', TransferAreas[0].data);
+	        	fillStaticData('LegalCharacters', LegalCharacters[0].data);
+	        	fillStaticData('SubnetTypes', SubnetTypes[0].data);
+	        	fillStaticData('CircuitTypes', CircuitTypes[0].data);
         		
         		var strData = JSON.stringify(staticData);
+        		$("#txtNoteStaticData").removeClass("hidden");
 
         		$.ajax({
         			type: "POST",
@@ -89,12 +110,15 @@ $(document).ready(function() {
         		})
         		.done(function( msg ) {
         			alert( msg );
+
+        			kui_txtStaticData.value(strData);
         		});
 
 		}) // DONE-->
 		.progress(function(d){
-			console.log(d);
+
 		})
+		.then(callbackSuccessGetStaticData, callbackFailGetStaticData)
 		.always(function(){
 			$("#btnSyncStaticData").button('reset');
 		});
