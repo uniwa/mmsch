b7a6de1dc452e231e5fad35e7bf4624e309e9068
Tsiolis Konstantinos <tsiolis.kostas@gmail.com>
2015-04-22 20:42:47 +0300

    commented entitySetAssociationOld,entitySetParam functions

--- server/classes/CRUDUtils.php
+++ server/classes/CRUDUtils.php
@@ -1,47 +1,47 @@
 <?php
 class CRUDUtils {
-    public static function entitySetAssociationOld(&$entity, $param, $repo, $field, $exceptionType, $required = true) {
-        global $entityManager;
-        $missingParam = 'Missing'.$exceptionType.'Param';
-        $missingValue = 'Missing'.$exceptionType.'Value';
-        $invalidType = 'Invalid'.$exceptionType.'Type';
-        $invalidValue = 'Invalid'.$exceptionType.'Value';
-
-        if ( $param === _MISSED_ ) {
-            if(!$required) { return; }
-            throw new Exception(constant('ExceptionMessages::'.$missingParam), constant('ExceptionCodes::'.$missingParam));
-        } else if ( Validator::IsNull($param) ) {
-            if(!$required) { return; }
-            throw new Exception(constant('ExceptionMessages::'.$missingValue), constant('ExceptionCodes::'.$missingValue));
-        } else if ( Validator::IsID($param) )
-            $retrievedObject = $entityManager->getRepository($repo)->find(Validator::ToID($param));
-        else if ( Validator::IsValue($param) )
-            $retrievedObject = $entityManager->getRepository($repo)->findOneBy(array('name' => Validator::ToValue($param)));
-        else
-            throw new Exception(constant('ExceptionMessages::'.$invalidType), constant('ExceptionCodes::'.$invalidType));
-
-        if ( !isset($retrievedObject) )
-            throw new Exception(constant('ExceptionMessages::'.$invalidValue), constant('ExceptionCodes::'.$invalidValue));
-        else
-        {
-            $method = 'set'.ucfirst($field);
-            $entity->$method($retrievedObject);
-        }
-    }
-
-    public static function entitySetParamOld(&$entity, $param, $exceptionType, $field) {
-        if ( $param === _MISSED_ )
-        { } //throw new Exception(ExceptionMessages::MissingNameParam, ExceptionCodes::MissingNameParam);
-        else if ( Validator::IsNull($param) )
-        { } //throw new Exception(ExceptionMessages::MissingNameValue, ExceptionCodes::MissingNameValue);}
-        else if ( Validator::IsValue($param) )
-        {
-            $method = 'set'.self::to_camel_case($field, true);
-            $entity->$method(Validator::ToValue($param));
-        }
-        else
-            throw new Exception($exceptionType." : ".$param, $exceptionType);
-    }
+//    public static function entitySetAssociationOld(&$entity, $param, $repo, $field, $exceptionType, $required = true) {
+//        global $entityManager;
+//        $missingParam = 'Missing'.$exceptionType.'Param';
+//        $missingValue = 'Missing'.$exceptionType.'Value';
+//        $invalidType = 'Invalid'.$exceptionType.'Type';
+//        $invalidValue = 'Invalid'.$exceptionType.'Value';
+//
+//        if ( $param === _MISSED_ ) {
+//            if(!$required) { return; }
+//            throw new Exception(constant('ExceptionMessages::'.$missingParam), constant('ExceptionCodes::'.$missingParam));
+//        } else if ( Validator::IsNull($param) ) {
+//            if(!$required) { return; }
+//            throw new Exception(constant('ExceptionMessages::'.$missingValue), constant('ExceptionCodes::'.$missingValue));
+//        } else if ( Validator::IsID($param) )
+//            $retrievedObject = $entityManager->getRepository($repo)->find(Validator::ToID($param));
+//        else if ( Validator::IsValue($param) )
+//            $retrievedObject = $entityManager->getRepository($repo)->findOneBy(array('name' => Validator::ToValue($param)));
+//        else
+//            throw new Exception(constant('ExceptionMessages::'.$invalidType), constant('ExceptionCodes::'.$invalidType));
+//
+//        if ( !isset($retrievedObject) )
+//            throw new Exception(constant('ExceptionMessages::'.$invalidValue), constant('ExceptionCodes::'.$invalidValue));
+//        else
+//        {
+//            $method = 'set'.ucfirst($field);
+//            $entity->$method($retrievedObject);
+//        }
+//    }
+//
+//    public static function entitySetParamOld(&$entity, $param, $exceptionType, $field) {
+//        if ( $param === _MISSED_ )
+//        { } //throw new Exception(ExceptionMessages::MissingNameParam, ExceptionCodes::MissingNameParam);
+//        else if ( Validator::IsNull($param) )
+//        { } //throw new Exception(ExceptionMessages::MissingNameValue, ExceptionCodes::MissingNameValue);}
+//        else if ( Validator::IsValue($param) )
+//        {
+//            $method = 'set'.self::to_camel_case($field, true);
+//            $entity->$method(Validator::ToValue($param));
+//        }
+//        else
+//            throw new Exception($exceptionType." : ".$param, $exceptionType);
+//    }
 
     private static function to_camel_case($str, $capitalise_first_char = false) {
         if($capitalise_first_char) {
