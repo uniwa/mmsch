0c534a999d33347820031f70ec5a8ec69633a72b
Tsiolis Konstantinos <tsiolis.kostas@gmail.com>
2015-08-05 16:08:02 +0300

    added a new case to DeleteExtLogEntries Function

--- api/delete/DeleteExtLog.php
+++ api/delete/DeleteExtLog.php
@@ -15,41 +15,59 @@ function DeleteExtLog() {
 
     try {
 
-//$page - $pagesize - $ordertype - $orderby ======================
+//$page - $pagesize - $ordertype - $orderby ====================================
        $page = 1;
        $pagesize = 1000;
        $ordertype =  Filters::getOrderType($ordertype, $params);
        $orderby = "el.id";
 
-//DELETION CASE1 
+//DELETION CASE1 (lastUpdate)
 
-    ////lastUpdate====================================================================
-    //        CRUDUtils::setSearchFilter($qb, 'a:1:{s:10:"lastUpdate"', "el", "data", 'STARTWITH', ExceptionMessages::InvalidUnitLastUpdateType, ExceptionCodes::InvalidUnitLastUpdateType);    
+        //lastUpdate============================================================
+        CRUDUtils::setSearchFilter($qb, 'a:1:{s:10:"lastUpdate"', "el", "data", 'STARTWITH', ExceptionMessages::InvalidUnitLastUpdateType, ExceptionCodes::InvalidUnitLastUpdateType);    
   
-//DELETION CASE2
+//DELETION CASE2 (studentsCount,groupsCount,levelsCount)
 
-    ////studentsCount=================================================================
-    //        CRUDUtils::setSearchFilter($qb, 'a:4:{s:13:"studentsCount"', "el", "data", 'STARTWITH', ExceptionMessages::InvalidUnitStudentsCountType, ExceptionCodes::InvalidUnitStudentsCountType);    
-    //        
-    ////groupsCount=================================================================
-    //        CRUDUtils::setSearchFilter($qb, 's:11:"groupsCount"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitGroupsCountType, ExceptionCodes::InvalidUnitGroupsCountType);  
-    //        
-    ////levelsCount=================================================================
-    //        CRUDUtils::setSearchFilter($qb, 's:11:"levelsCount"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitLevelsCountType, ExceptionCodes::InvalidUnitLevelsCountType);  
-    //        
-    ////lastUpdate====================================================================
-    //        CRUDUtils::setSearchFilter($qb, 's:10:"lastUpdate"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitLastUpdateType, ExceptionCodes::InvalidUnitLastUpdateType); 
+        //studentsCount=========================================================
+        //CRUDUtils::setSearchFilter($qb, 'a:3:{s:13:"studentsCount"', "el", "data", 'STARTWITH', ExceptionMessages::InvalidUnitStudentsCountType, ExceptionCodes::InvalidUnitStudentsCountType);    
+
+        //groupsCount===========================================================
+        //CRUDUtils::setSearchFilter($qb, 's:11:"groupsCount"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitGroupsCountType, ExceptionCodes::InvalidUnitGroupsCountType);  
+
+        //levelsCount===========================================================
+        //CRUDUtils::setSearchFilter($qb, 's:11:"levelsCount"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitLevelsCountType, ExceptionCodes::InvalidUnitLevelsCountType); 
     
-//DELETION CASE3
+//DELETION CASE3 (studentsCount,groupsCount,levelsCount,lastUpdate)
 
-    //studentsCount=================================================================
-            CRUDUtils::setSearchFilter($qb, 'a:3:{s:13:"studentsCount"', "el", "data", 'STARTWITH', ExceptionMessages::InvalidUnitStudentsCountType, ExceptionCodes::InvalidUnitStudentsCountType);    
+        //studentsCount=========================================================
+        //CRUDUtils::setSearchFilter($qb, 'a:4:{s:13:"studentsCount"', "el", "data", 'STARTWITH', ExceptionMessages::InvalidUnitStudentsCountType, ExceptionCodes::InvalidUnitStudentsCountType);
 
-    //groupsCount=================================================================
-            CRUDUtils::setSearchFilter($qb, 's:11:"groupsCount"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitGroupsCountType, ExceptionCodes::InvalidUnitGroupsCountType);  
+        //groupsCount===========================================================
+        //CRUDUtils::setSearchFilter($qb, 's:11:"groupsCount"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitGroupsCountType, ExceptionCodes::InvalidUnitGroupsCountType);  
 
-    //levelsCount=================================================================
-            CRUDUtils::setSearchFilter($qb, 's:11:"levelsCount"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitLevelsCountType, ExceptionCodes::InvalidUnitLevelsCountType);  
+        //levelsCount===========================================================
+        //CRUDUtils::setSearchFilter($qb, 's:11:"levelsCount"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitLevelsCountType, ExceptionCodes::InvalidUnitLevelsCountType);  
+
+        //lastUpdate============================================================
+        //CRUDUtils::setSearchFilter($qb, 's:10:"lastUpdate"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitLastUpdateType, ExceptionCodes::InvalidUnitLastUpdateType); 
+    
+//DELETION CASE5 (areaTeamNumber,studentsCount,groupsCount,levelsCount,lastUpdate)
+        
+        //areaTeamNumber========================================================
+        //CRUDUtils::setSearchFilter($qb, 'a:5:{s:14:"areaTeamNumber"', "el", "data", 'STARTWITH', ExceptionMessages::InvalidUnitAreaTeamNumberType, ExceptionCodes::InvalidUnitAreaTeamNumberType);
+        
+        //studentsCount=========================================================
+        //CRUDUtils::setSearchFilter($qb, 's:13:"studentsCount"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitStudentsCountType, ExceptionCodes::InvalidUnitStudentsCountType);
+
+        //groupsCount===========================================================
+        //CRUDUtils::setSearchFilter($qb, 's:11:"groupsCount"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitGroupsCountType, ExceptionCodes::InvalidUnitGroupsCountType);  
+
+        //levelsCount===========================================================
+        //CRUDUtils::setSearchFilter($qb, 's:11:"levelsCount"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitLevelsCountType, ExceptionCodes::InvalidUnitLevelsCountType);  
+
+        //lastUpdate============================================================
+        //CRUDUtils::setSearchFilter($qb, 's:10:"lastUpdate"', "el", "data", 'CONTAINALL', ExceptionMessages::InvalidUnitLastUpdateType, ExceptionCodes::InvalidUnitLastUpdateType); 
+
        
 //execution=====================================================================
         $qb->select('el');
@@ -66,14 +84,16 @@ function DeleteExtLog() {
 //data results==================================================================
         $count = 0;
         $entries = array();
-        $required_keys = array("studentsCount","groupsCount","levelsCount");
+
         foreach ($results as $extlog)
         {
             $unit_data = unserialize($extlog->getData());
             
-//       if ((count($unit_data)== 1) && (key_exists('lastUpdate', $unit_data))) {
-//       if ((count($unit_data)== 4) && (count(array_diff($required_keys, array_keys($unit_data))) == 0)) { 
-         if ((count($unit_data)== 3) && (count(array_diff($required_keys, array_keys($unit_data))) == 0)) {                
+      if ((count($unit_data)== 1) && (key_exists('lastUpdate', $unit_data))) {
+//      if ((count($unit_data)== 3) && (count(array_diff(array("studentsCount","groupsCount","levelsCount"), array_keys($unit_data))) == 0)) { 
+//      if ((count($unit_data)== 4) && (count(array_diff(array("studentsCount","groupsCount","levelsCount","lastUpdate"), array_keys($unit_data))) == 0)) { 
+//      if ((count($unit_data)== 5) && (count(array_diff(array("areaTeamNumber","studentsCount","groupsCount","levelsCount","lastUpdate"), array_keys($unit_data))) == 0)) { 
+           
             $result["data"][] = array(
                                         "id"            => $extlog->getId(),
                                         "action"        => $extlog->getAction(),
@@ -97,7 +117,7 @@ function DeleteExtLog() {
        foreach ($entries as $entry){
            $result["comments"][] = 'Delete entry with id = '.$entry;
            $DeleteExtLogEntry = $entityManager->find('ExtLogEntries', $entry);
-           //delete from db================================================================
+           //delete from db=====================================================
 //           $entityManager->remove($DeleteExtLogEntry);
 //           $entityManager->flush($DeleteExtLogEntry);
        }
