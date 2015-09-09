c98d2d65ad699ed9f3957424171b53da67ddbec7
Tsiolis Konstantinos <tsiolis.kostas@gmail.com>
2015-09-09 12:51:25 +0300

    added throw exception

--- server/system/connection.php
+++ server/system/connection.php
@@ -1,9 +1,13 @@
 <?php
 
-$db = new PDO( "mysql:host=".$conOptions["Host"]."; port=".$conOptions["Port"]."; dbname=".$conOptions["Database"], $conOptions["Username"], $conOptions["Password"] );
+try {
+    $db = new PDO( "mysql:host=".$conOptions["Host"]."; port=".$conOptions["Port"]."; dbname=".$conOptions["Database"], $conOptions["Username"], $conOptions["Password"] );
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->query("SET NAMES UTF8");
 
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$db->query("SET NAMES UTF8");
+} catch (PDOException $e) { 
+    echo "Connection failed with mysql server ";// . $e->getMessage();
+    exit;
+}
 
 ?>
